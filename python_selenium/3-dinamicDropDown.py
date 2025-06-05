2f61482d2b8f8449aca568c91ab6471c4fcf6b55
Cristian <cristiandavid1291@gmail.com>
2025-06-05 14:59:47 -0500

    dinamic drop down v2

--- python_selenium/3-dinamicDropDown.py
+++ python_selenium/3-dinamicDropDown.py
@@ -8,3 +8,10 @@ time.sleep(2)
 suggestions =  driver.find_elements("css selector", "li.ui-menu-item a")
 print(suggestions)
 next(suggestion.click() for suggestion in suggestions if suggestion.text == "India")
+
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+checkboxes = driver.find_elements("css selector", "input[id*='checkBox']")
+
+next(checkbox.click() for checkbox in checkboxes if checkbox.get_attribute("value") == "option2")
+time.sleep(2)
